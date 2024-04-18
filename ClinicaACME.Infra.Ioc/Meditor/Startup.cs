7679848b829499855f66190058b881984ce17ab7
Jose Rodrigo <jose.rodrigo.vargas@autocom-mg.com.br>
2024-04-18 15:49:13 -0300

    - Adicionado serviço de injeção de depêdencia do meditor.
    - Criado a classe ValidationBehavior para executar as validações antes de utlizados os manipuladores do mediator.

---
+++ ClinicaACME.Infra.Ioc/Meditor/Startup.cs
@@ -0,0 +1,36 @@
+﻿
+
+using ClinicaACME.Application.Behaviors;
+using ClinicaACME.Application.Commands.Request.Patient;
+using ClinicaACME.Application.Commands.Response.Patient;
+using ClinicaACME.Application.Handlers.PatientHandler;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace ClinicaACME.Infra.Ioc.Meditor
+{
+    internal static class Startup
+    {
+        internal static IServiceCollection AddServiceMediator(this IServiceCollection services)
+        {
+
+            services.AddScoped<IRequestHandler<CreatePatientRequest, CreatePatientResponse>, CreatePatientHandler>();
+            services.AddScoped<IRequestHandler<UpdatePatientRequest, UpdatePatientResponse>, UpdatePatientHandler>();
+            //services.AddScoped<IRequestHandler<, IEnumerable<>>, >();
+            services.AddScoped<IRequestHandler<GetParientByNameRequest, GetPatientByNameResponse>, GetPatientByNameHandler>();
+            services.AddScoped<IRequestHandler<DeletePatientRequest, DeletePatientResponse>, DeletePatientHandler>();
+
+
+
+            // Configura serviço do MediatR e registra os handlers no assembly atual
+            services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+                cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+            });
+
+            return services;
+        }
+    }
+}
