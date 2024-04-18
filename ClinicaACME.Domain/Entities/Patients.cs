15d5d1f9138e7362bd2bc9c024b43d30b9a9c68c
Jose Rodrigo <jose.rodrigo.vargas@autocom-mg.com.br>
2024-04-18 12:59:44 -0300

    - Criado solução ClinicaACME.Base.
    - Adiciondo a interface IRepositoryBase, e métodos basícos para realizar de 'CRUD'.

--- ClinicaACME.Domain/Entities/Patients.cs
+++ ClinicaACME.Domain/Entities/Patients.cs
@@ -1,9 +1,4 @@
 ﻿using ClinicaACME.Domain.Entities.Commons;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ClinicaACME.Domain.Entities
 {
---
+++ ClinicaACME.Domain/Interfaces/IRespositoryBase.cs
@@ -0,0 +1,12 @@
+﻿
+namespace ClinicaACME.Domain.Interfaces
+{
+    public interface IRespositoryBase<TEntity>
+    {
+        Task<TEntity> Create(TEntity entity);
+        Task<int> Update(TEntity entity);
+        Task<IEnumerable<TEntity>> GetAll(TEntity entity);
+        Task<TEntity> GetByName(string name);
+        Task<int> Delete(TEntity entity);
+    }
+}
