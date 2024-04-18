476d437d3e312de5d67e268d92fad252deec3bc9
Jose Rodrigo <jose.rodrigo.vargas@autocom-mg.com.br>
2024-04-18 16:23:47 -0300

    - Adicinado serviço de injeção de dependência para utilizando do fluent validation.

---
+++ ClinicaACME.Infra.Ioc/FluentValidation/Startup.cs
@@ -0,0 +1,19 @@
+﻿using ClinicaACME.Application.Validations.PatientValidations;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ClinicaACME.Infra.Ioc.FluentValidation
+{
+    internal static class Startup
+    {
+        internal static IServiceCollection AddServiceValidationDomains(this IServiceCollection services)
+        {
+            services.AddValidatorsFromAssemblyContaining<CreatePatientValidation>();
+            services.AddValidatorsFromAssemblyContaining<UpdatePatientValidation>();
+            services.AddValidatorsFromAssemblyContaining<GetPatientByNameValidation>();
+            services.AddValidatorsFromAssemblyContaining<DeletePatientValidation>();
+
+            return services;
+        }
+    }
+}
