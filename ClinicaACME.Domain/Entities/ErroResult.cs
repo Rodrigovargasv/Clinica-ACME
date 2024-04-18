f1cedb8ef3b192bfb73bcf203a2783239a48524b
Jose Rodrigo <jose.rodrigo.vargas@autocom-mg.com.br>
2024-04-18 14:28:54 -0300

    - Adicionado classe ErroResult para respresentação dos erros enviado pela api.

---
+++ ClinicaACME.Domain/Entities/ErroResult.cs
@@ -0,0 +1,12 @@
+﻿
+namespace ClinicaACME.Domain.Entities
+{
+    public class ErroResult
+    {
+        public List<string> Messages { get; set; } = new();
+        public string Message { get; set; }
+        public string? Source { get; set; }
+        public string? Exception { get; set; }
+        public int StatusCode { get; set; }
+    }
+}
