6784a2c1486a51c7bf673fcb05cac5b43d0bf107
Jose Rodrigo <jose.rodrigo.vargas@autocom-mg.com.br>
2024-04-18 14:14:03 -0300

    - Adicionado configuração de injeção de depência do contexto de banco de dados em memória.

---
+++ ClinicaACME.Infra.Ioc/Context/Startup.cs
@@ -0,0 +1,21 @@
+﻿
+using ClinicaACME.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ClinicaACME.Infra.Ioc.Context
+{
+    internal static class Startup
+    {
+        internal static IServiceCollection AddServiceDBContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseInMemoryDatabase("ClinicaACME");
+            });
+
+            return services;
+        }
+    }
+}
---
+++ ClinicaACME.Infra.Ioc/StartupBase.cs
@@ -0,0 +1,17 @@
+﻿
+using ClinicaACME.Infra.Ioc.Context;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ClinicaACME.Infra.Ioc
+{
+    public static class StartupBase
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddServiceDBContext(configuration);
+
+            return services;
+        }
+    }
+}
